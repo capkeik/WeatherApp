d5aa5d9706228ae1afa330f1c7a8d5f37f3b5b99
capkeik <new.mail2018@mail.ru>
2022-09-03 03:03:19 +0300

    add weather repository

---
+++ app/src/main/java/com/example/weatherapp/data/repository/WeatherRepository.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapp.data.repository
+
+import com.example.weatherapp.data.api.ApiService
+import com.example.weatherapp.data.entity.CityInfo
+import com.example.weatherapp.data.mapper.CityInfoMapper
+
+class WeatherRepository {
+    val api: ApiService = ApiService
+    val mapper: CityInfoMapper = CityInfoMapper()
+
+    suspend fun getWeatherList(lat: Double, lon: Double, count: Int): List<CityInfo> {
+        val oldList = api.weatherApi.getWeatherList(lat, lon, count)
+        return List(10) {
+            mapper(oldList.list[it])
+        }
+    }
+
+    suspend fun getWeather(id: Int) =
+        mapper(
+            api.weatherApi.getWeather(id)
+        )
+
+
+    suspend fun getWeather(name: String) =
+        mapper(
+            api.weatherApi.getWeather(name)
+        )
+
+}
